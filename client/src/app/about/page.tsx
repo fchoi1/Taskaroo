0923bd683fa24de026059922b21e7284aef342fe
fchoi1 <fchoi@outlook.com>
2023-06-06 15:52:59 -0400

    Fix TS errors

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -2,11 +2,6 @@
 
 import { NextPage } from "next";
 
-import Sidebar from "../../components/Sidebar";
-import CardComponent from "../../components/common/Card";
-import Button from "../../components/common/Button";
-import StatusColumn from "../../components/common/StatusColumn";
-import Modal from "../../components/common/Modal";
 import Link from "next/link";
 
 interface AboutPage {
@@ -16,17 +11,6 @@ interface AboutPage {
 const About: NextPage<AboutPage> = () => {
   return (
     <div>
-      <Sidebar name="Name" />
-      <CardComponent onClick={() => {}}></CardComponent>
-      <StatusColumn />
-      {/* <Modal title="Modal title"></Modal> */}
-      <Button
-        onClick={() => {
-          console.log("clicked button here");
-        }}
-      >
-        Button
-      </Button>
       <Link href="/">main</Link>
       <div> This is the about with stuff</div>;
     </div>
