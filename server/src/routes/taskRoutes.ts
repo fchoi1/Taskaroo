99419ac29914396b2f8c61606a923feeecae655f
fchoi1 <fchoi@outlook.com>
2023-06-23 15:54:25 -0400

    Started making backend server

--- server/src/routes/taskRoutes.ts
+++ server/src/routes/taskRoutes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import * as express from 'express';
 import taskController from '../controllers/taskController';
 
 const router = express.Router();
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import * as express from 'express';
 import taskRoutes from './routes/taskRoutes';
 
 const app = express();
